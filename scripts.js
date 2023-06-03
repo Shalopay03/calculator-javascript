236eae2d35b04d6c51de5c5b31623a7d992dfb7f
Shalopay03 <134221829+Shalopay03@users.noreply.github.com>
2023-06-03 21:25:36 +0300

    add operate function

--- scripts.js
+++ scripts.js
@@ -16,7 +16,19 @@ function divide(a, b){
     return a / b;
 }
 
+//calls needed operation function
+function operate(firstNumber, secondNumber, operator){
+    if(operator==="+") return add(firstNumber, secondNumber,);
+    if(operator==="-") return subtract(firstNumber, secondNumber,);
+    if(operator==="*") return multiply(firstNumber, secondNumber,);
+    if(operator==="/") return divide(firstNumber, secondNumber,);
+    return "ERROR";
+}
+
 //basic variables
-let firstNumber=0;
-let secondNumber=0;
-let operation = '';
+let firstNumber=10;
+let secondNumber=5;
+let operator = '+';
+
+//test
+console.log(operate(firstNumber, secondNumber, operator));
